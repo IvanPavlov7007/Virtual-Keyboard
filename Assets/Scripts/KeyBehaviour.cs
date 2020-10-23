db2ad28c78ffba900c593bd130ab527e78ecd84d
IvanPavlov <ivanpavlov7007@gmail.com>
2020-10-23 21:41:43 +0200

    Initial commit

---
+++ Assets/Scripts/KeyBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[ExecuteAlways]
+public class KeyBehaviour : MonoBehaviour
+{
+    TextMeshPro textDisplayer;
+    PhysicalKeyResponse physicalResponse;
+    public string actualKeyboardKey;
+
+    void Start()
+    {
+        textDisplayer = GetComponentInChildren<TextMeshPro>();
+        physicalResponse = GetComponent<PhysicalKeyResponse>();
+        textDisplayer.text = actualKeyboardKey.ToUpper();
+    }
+
+    void Update()
+    {
+#if UNITY_EDITOR
+        textDisplayer.text = actualKeyboardKey.ToUpper();
+#endif
+        if (Input.GetKeyDown(actualKeyboardKey))
+        {
+            physicalResponse.Press();
+            Debug.Log(actualKeyboardKey);
+        }
+        else if (Input.GetKeyUp(actualKeyboardKey))
+            physicalResponse.Release();
+    }
+}
---
+++ Assets/Scripts/PhysicalKeyResponse.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicalKeyResponse : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 pressedDirection;
+    [SerializeField]
+    float pressAnimationSpeed = 20f, releaseAnimationSpeed = 60f,
+        inclineFrameY = 5f, inclineFrameZMultipluier = 3f;
+
+    [SerializeField]
+    bool controlledByAxis = false;
+
+    [SerializeField]
+    AudioClip pressSound, releaseSound;
+
+    Vector3 pressedPosition;
+
+    bool pressed;
+    Coroutine pressAnimationCoroutine;
+    Quaternion endRotation;
+
+    //Box-edges in its local space
+    const float maxX = 0.5f;
+    const float maxZ = 0.5f;
+    protected void Awake()
+    {
+        pressedPosition = transform.localPosition + pressedDirection;
+    }
+
+    //Some work in progress
+    //public void controlByAxis(float axisValue)
+    //{
+    //    if (!pressed && axisValue > 0)
+    //    {
+    //        controlledByAxis = true;
+
+    //        Vector3 pressPoint = new Vector3(Random.Range(-maxX, maxX), 0, Random.Range(-maxZ, maxZ));
+    //        Press(pressPoint);
+    //    }
+    //    else if (controlledByAxis && pressed && axisValue == 0)
+    //    {
+    //        pressed = false;
+    //        controlledByAxis = false;
+    //    }
+    //}
+
+    public void Press(Vector3 point)
+    {
+        //base.Press(point);
+        //aud.PlayOneShot(pressSound);
+        calculateEndRotation(point);
+        if (pressAnimationCoroutine == null)
+            pressAnimationCoroutine = StartCoroutine(PressAnimation());
+        else
+        {
+            pressed = true;
+        }
+    }
+
+    public void Press()
+    {
+        Press(transform.up);
+    }
+
+    public void Release()
+    {
+        if (pressed)
+        {
+            pressed = false;
+        }
+    }
+
+    //Some work in progress
+
+    //protected override void handleMouseUp(RaycastHit[] hits)
+    //{
+    //    base.handleMouseUp(hits);
+    //    if (pressed)
+    //    {
+    //        aud.PlayOneShot(releaseSound);
+    //        pressed = false;
+    //    }
+    //}
+
+    //protected override void handleNotAlteredRaycast(RaycastHit[] hits)
+    //{
+    //    base.handleNotAlteredRaycast(hits);
+    //    if (controlledByAxis)
+    //        return;
+    //    if (pressed)
+    //    {
+    //        RaycastHit hit = System.Array.Find(hits, obj => obj.transform == transform);
+    //        if (hit.transform != null)
+    //        {
+    //            calculateEndRotation(transform.InverseTransformPoint(hit.point));
+    //        }
+    //        else
+    //            pressed = false;
+    //    }
+    //}
+
+    private void calculateEndRotation(Vector3 pressPoint)
+    {
+        pressPoint.y += inclineFrameY;
+        pressPoint.z *= inclineFrameZMultipluier;
+        endRotation = transform.rotation * Quaternion.FromToRotation(transform.up * inclineFrameY, pressPoint);
+    }
+
+    IEnumerator PressAnimation()
+    {
+        float animationState = 0;
+        Quaternion defaultRotation = transform.localRotation;
+        Vector3 defaultPosition = transform.localPosition;
+        pressed = true;
+        do
+        {
+            animationState += (pressed ? Time.deltaTime * pressAnimationSpeed : -Time.deltaTime * releaseAnimationSpeed);
+            animationState = Mathf.Min(2f, animationState);
+            transform.localRotation = Quaternion.Slerp(defaultRotation, endRotation, animationState);
+            transform.localPosition = Vector3.Lerp(defaultPosition, pressedPosition, Mathf.Clamp(animationState, 1f, 2f) - 1f);
+            yield return new WaitForEndOfFrame();
+        } while (animationState > 0);
+        pressAnimationCoroutine = null;
+        yield return 0;
+    }
+}
